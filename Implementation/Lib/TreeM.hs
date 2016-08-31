6d297b842e86ca28616823902c4fdfe7fcc653aa
paolino <paolo.veronelli@gmail.com>
2016-08-31 18:27:11 +0200

    .

--- Implementation/Lib/TreeM.hs
+++ Implementation/Lib/TreeM.hs
@@ -5,36 +5,42 @@ import Data.List
 import Data.Ord
 import Control.Arrow
 import qualified Data.Tree as T
--- import qualified Data.Map as M 
+import qualified Data.IntMap as M 
 
 
 -- type Up a = M.Map a (Int,a)
+type L a = M.IntMap a
 
-data T a = T a [T a] deriving (Show, Functor)
+data T a = T a (L (T a)) deriving (Show, Functor)
 
-convert :: T a -> T.Tree a
-convert (T x xs) = T.Node x $ map convert xs
+convert :: (Maybe Int,T a) -> T.Tree (Maybe Int,a)
+convert (n,T x xs) = T.Node (n,x) $ map (convert . first Just) $ M.assocs xs
 
-drawT :: (a -> String) -> T a -> String
-drawT f = T.drawTree . convert . fmap f
+drawT :: ((Maybe Int,a) -> String) -> T a -> String
+drawT f = T.drawTree . fmap f . convert . (,) Nothing
 
-type P a = [a]
 
-type R = [Int]
+data A a = D | C a | A a
 
-pick :: Int -> [a] -> (Maybe a -> [a] , a)
+pick :: Int ->  L a -> (A a -> (L a, Int) , a)
 pick n xs = let 
-    (bs,x:rs) = splitAt n xs
-    in ((\f -> bs ++ f rs) . maybe id (:), x)
+    (succ -> m) = fst $ M.findMax xs 
+    x = xs M.! n
+    f D = (xs,n)
+    f (C x) = (M.insert n x xs, n)
+    f (A x) = (M.insert m x xs, m)
+    in (f , x)
  
+type R = [Int]
 reRoot = reRoot' id where
     reRoot' :: (T a -> T a) -> T a -> R -> T a
     reRoot' f t [] = f t
     reRoot' f (T x ts) (p:ps) = let 
-        (($ Nothing) -> ts', c) = pick p ts
+        (r , c) = pick p ts
         g (T y ts) =  T y $ T x ts' : ts
         in reRoot' g c ps
 
+{-
 cut :: T a -> R -> (T a ,T a)
 cut (T x ts) [p] = first (T x . ($ Nothing)) $ pick p ts
 cut (T x ts) (p:ps) = let 
@@ -47,10 +53,11 @@ link t r (T x ts) (p:ps) = let
     (s,c) = pick p ts
     in T x . s . Just $ link t r c ps 
 
+type P a = [a]
 paths :: T a -> [P a]
 paths (T x []) = [[x]]
 paths (T x xs) = (x:p):ps where
     p:ps = sortBy (comparing length) $ xs >>= paths 
-
+-}
 
     
