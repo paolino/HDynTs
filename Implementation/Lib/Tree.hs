55e827dfc34a3565062583e4f34e27b5781a5d98
paolino <paolo.veronelli@gmail.com>
2016-09-06 16:36:37 +0200

    naive aux map

--- Implementation/Lib/Tree.hs
+++ Implementation/Lib/Tree.hs
@@ -12,29 +12,43 @@ import qualified Data.Tree as T
 
 data T a = T a [T a] deriving (Show, Functor)
 
-convert :: T a -> T.Tree a
-convert (T x xs) = T.Node x $ map convert xs
-
-drawT :: (a -> String) -> T a -> String
-drawT f = T.drawTree . convert . fmap f
-
 type P a = [a]
 
 type R = [Int]
 
-pick :: Int -> [a] -> (Maybe a -> [a] , a)
+pick :: Int -> [a] -> (a -> [a] , a)
 pick n xs = let 
     (bs,x:rs) = splitAt n xs
-    in ((\f -> bs ++ f rs) . maybe id (:), x)
+    in ((\x -> bs ++ x:rs), x)
  
-reRoot = reRoot' id where
-    reRoot' :: (T a -> T a) -> T a -> R -> T a
-    reRoot' f t [] = f t
-    reRoot' f (T x ts) (p:ps) = let 
-        (($ Nothing) -> ts', c) = pick p ts
-        g (T y ts) =  T y $ T x ts' : ts
-        in reRoot' g c ps
+type B = ([(R,R)],R)
 
+
+
+prefixSubst :: ([(R,R)],[(R,R)]) -> R -> R
+prefixSubst = undefined
+
+reRoot :: T a -> R -> (T a, R -> R)
+reRoot (T x ts) (p:ps) =
+    let     (bs,y:rs) = splitAt p $ ts
+            t' = T x (bs ++ rs)
+            (t'',(cs,acc))  = reRootInside t' y ps [p]
+            ms = (if (p == 0) then [] else (map (\n -> ([n],reverse $ n:acc)) [0..p-1])) 
+                ++ map (\n -> ([n],reverse $ (n - 1) :acc)) [p+1..length ts-1]
+            correct [] = reverse acc
+            correct xs = prefixSubst (ms,cs) xs
+    in (t'', correct)
+
+
+reRootInside :: T a -> T a -> R -> R -> (T a, B)
+reRootInside t' (T x ts) [] rp = (T x $ ts ++ [t'], ([(reverse rp,[])], [length ts]))
+reRootInside t' (T x ts) (p:ps) rp = let 
+    (r, c) = pick p ts
+    t'' = T x $ r t'
+    h (cs,acc) = ((reverse rp, reverse acc) : cs , p:acc) 
+    second h $ reRootInside t'' c ps (p:rs)
+
+{-
 cut :: T a -> R -> (T a ,T a)
 cut (T x ts) [p] = first (T x . ($ Nothing)) $ pick p ts
 cut (T x ts) (p:ps) = let 
@@ -52,5 +66,11 @@ paths (T x []) = [[x]]
 paths (T x xs) = (x:p):ps where
     p:ps = sortBy (comparing length) $ xs >>= paths 
 
-
+-}
     
+convert :: T a -> T.Tree a
+convert (T x xs) = T.Node x $ map convert xs
+
+drawT :: (a -> String) -> T a -> String
+drawT f = T.drawTree . convert . fmap f
+
