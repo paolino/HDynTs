f25a7eab45f5d3b57209b17df2d2c8a9f9bf1f94
paolino <paolo.veronelli@gmail.com>
2016-09-12 11:20:50 +0200

    example

---
+++ LinkCut/Test.hs
@@ -0,0 +1,21 @@
+dbS = mkRMap (IString 0) :: Db String
+
+t1 = let 
+    (db',k) = new dbS "paolino"
+    in query db' k == "paolino"
+t2 = let 
+    (db1,k) = new dbS "paolino"
+    db2 = update db1 "juan"
+    in query db2 k == "juan"
+
+
+newtype DbSingle m a b = DbSingle (StateT (Db a) m b) deriving 
+    (Monad, MonadState)
+
+testMonadic :: DbSingle m String Bool
+testMonadic = do
+    k <- new' "paolino"
+    r <- query' k
+    update' r "juan"
+    return $ r == "juan"
+
