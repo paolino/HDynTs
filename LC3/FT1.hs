286b839d7b4cd4f68015aad35d45c9c5591ad9a3
paolino <paolo.veronelli@gmail.com>
2016-09-17 19:02:32 +0200

    lc3 link

--- LC3/FT1.hs
+++ LC3/FT1.hs
@@ -1,5 +1,6 @@
 {-# language MultiParamTypeClasses, TemplateHaskell, ScopedTypeVariables, 
-    ViewPatterns, FlexibleInstances,DeriveFunctor, StandaloneDeriving #-}
+    ViewPatterns, FlexibleInstances,DeriveFunctor, StandaloneDeriving, 
+    NoMonomorphismRestriction #-}
 import qualified Data.FingerTree as F
 import  Data.FingerTree (FingerTree, split, measure, Measured, viewl,viewr,  
     (<|) , (|>), ViewL ((:<)), ViewR ((:>)))
@@ -7,7 +8,7 @@ import qualified Data.Set as S
 import Data.IntMap (IntMap)
 import Data.Set (Set)
 import Data.List
-import Control.Arrow ((&&&), (***), second)
+import Control.Arrow ((&&&), (***), second, first )
 import Data.Maybe (isNothing, fromJust )
 import Control.Lens hiding ((:<),(<|),(|>),(:>), children)
 import Control.Lens.TH
@@ -28,6 +29,11 @@ change t [] = Nothing
 change t (x:xs) = case t x of
     Nothing -> (x :) <$>  change t xs
     Just x' -> Just $ x' : xs
+
+unfoldrE :: (b -> Either b (a,b)) -> b -> ([a],b)
+unfoldrE f x = case f x of
+    Left y -> ([],y)
+    Right (z,y) -> first (z:) $ unfoldrE f y
 
 --------------------------------------------------------------------------------
 ---------------------Sets as a measure for fingertrees--------------------------
@@ -58,16 +64,16 @@ instance Ord a => Monoid (IsoPath a) where
     mempty = IsoPath mempty mempty
 
 ------results of splitting an IsoPath ------------------------------------------
-data Split a = NotFound | Take  a | Splitted a a
+data Split a = Take a | Splitted a a
     deriving (Show, Functor)
 
-splitIsoPath :: Ord a => a -> IsoPath a -> Split (IsoPath a)
+splitIsoPath :: Ord a => a -> IsoPath a -> Maybe (Split (IsoPath a))
 splitIsoPath x p@(IsoPath o r) = let 
     (ao,bo) = split (S.member x) o
     (ar, viewl -> w :< br) = split (S.member x) r
-    in  if null bo then NotFound
-        else if null ao then Take p
-        else Splitted (IsoPath ao br) (IsoPath bo $ ar |> w )
+    in  if null bo then Nothing
+        else if null ao then Just $ Take p
+        else Just $ Splitted (IsoPath ao br) (IsoPath bo $ ar |> w )
 
 swapIsoPath :: IsoPath a -> IsoPath a
 swapIsoPath (IsoPath o r) = IsoPath r o
@@ -86,10 +92,10 @@ makeLenses ''Path
 type Forest b a = [b a]
 
 -- | convert a st of trees to a forest of paths
-toForest    :: Ord a 
+treesToPaths   :: Ord a 
             => Forest Tree a -- ^ tree to deconstruct
             -> Forest Path a -- ^ deconstruction
-toForest = concatMap (paths' Nothing) where
+treesToPaths = concatMap (paths' Nothing) where
     paths' m (Node x []) = [Path m (IsoPath ft ft)] where 
                      ft = F.singleton $ Collect x
     paths' m (Node x xs) = let 
@@ -128,73 +134,85 @@ maybeNode f c = case consume c of
             Just n -> second (n:) $ children (Just h) f'
         
 -- | convert a forest of paths back to forest of trees format
-fromForest  :: Ord a 
+pathsToTrees  :: Ord a 
             => Forest Path a    -- ^ paths to reconstruct
             ->  Forest Tree a   -- ^ reconstruction
-fromForest = map (fromJust <$>) . snd . children Nothing 
+pathsToTrees = map (fromJust <$>) . snd . children Nothing 
 
-        
-splitPath :: Ord a => a -> Path a -> Split (Path a)
-splitPath x p@(Path mf dp) = case splitIsoPath x dp of
-        Splitted p1 p2 -> 
-            Splitted (Path (Just x) p1) (Path mf p2)
-        Take p -> Take (Path mf p)
-        NotFound -> NotFound
+----------------------------------------------------------------
+
+-- split a path 
+splitPath :: Ord a => a -> Path a -> Maybe (Split (Path a))
+splitPath x p@(Path mf dp) = g <$> splitIsoPath x dp where
+    g (Splitted p1 p2) = Splitted (Path (Just x) p1) (Path mf p2)
+    g (Take p) = Take (Path mf p)
+
+unsplitPath (Splitted l r) = Path (view father r) (view path l <> view path r)
+unsplitPath (Take p) = p
 
 joinAndReversePaths :: Ord a => Forest Path a -> Path a
 joinAndReversePaths xs = over path (swapIsoPath) $ 
     foldr1 (\(Path _ dp) (Path mf dp') ->  Path mf $ dp <> dp') xs
 
----------------------------------------------------------------------
-
-{-
-
-data Result a = Result {
-    residual :: Path a,
-    taken :: Path a
-    }
-
-unzipResults :: [Result a] -> ([Path a], [Path a])
-unzipResults = unzip . map (\(Result r t)  -> (r,t))
-
-
 -- insert a path, trying to merge something depending on its tail
-mergeCorrection :: Ord a => Path a -> Forest a -> Maybe (Forest a)
+mergeCorrection :: Ord a => Path a -> Forest Path a -> Maybe (Forest Path a)
 mergeCorrection p = change $ \p' -> 
     case view father p' == Just (unCollect $ tailIsoPath $ view path p) of
         True -> Just $ over path (mappend $ view path p') p
         _ -> Nothing
 
-takePath :: Ord a => a -> Forest a -> (Either (Path a) (Result a), Forest a)
-takePath x f = let
-    failure NotFound = True
-    failure _ = False
-    ([(_,t)], unzip -> (f',_)) = partition (not . failure . snd) $ 
+mergeOrInsert :: Ord a => Path a -> Forest Path a -> Forest Path a
+mergeOrInsert p f = maybe (p:f) id $ mergeCorrection p f
+--------------------------------------------------------------------------------
+--------Linking-----------------------------------------------------------------
+-- linking two vertex or creating an edge---------------------------------------
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+
+-- result of a successful split
+data Result a = Result {
+    residual :: Path a,
+    taken :: Path a
+    }
+
+
+unzipResults :: [Result a] -> ([Path a], [Path a])
+unzipResults = unzip . map (\(Result r t)  -> (r,t))
+
+-- extract a splitted path from the forest given the splitting vertex
+selectPath :: Ord a => a -> Forest Path a -> (Split (Path a), Forest Path a)
+selectPath x f = let
+    ([(_,Just s)], unzip -> (f',_)) = partition (isJust . snd) $ 
         zip <*> map (splitPath x) $ f
-    in case t of
-        Take p -> (Left p, f') -- error "minimum number of path invariant broken"
-        Splitted r p -> (Right $ Result r p, f')
+    in (s,f')
 
-exposeIterate  :: Maybe a -> ([Result a],Forest a) -> ([Result a] , Forest a)
-exposeIterate Nothing y = y
-exposeIterate (Just x) (rs,f) = let
-    case takePath LC x f of
-        (Right (r@(Result _ (Path mf _))),f') -> exposeIterate mf (r:rs,f')       
-        _ -> error "invariant broken" 
+-- core expose function, cut and take the dependency of the right cutted piece
+-- and cut again, collecting results
+type ExposeCore a = (Maybe a , Forest Path a)
+exposeCore      :: Ord a 
+                => ExposeCore a -- actual results and
+                -> Either (ExposeCore a) (Result a, ExposeCore a) 
+exposeCore (Nothing,f) = Left (Nothing,f)
+exposeCore (Just x, f) = case selectPath x f of
+            (Splitted r l,f') -> Right (Result r l,(view father l,f'))
+            _ -> error "invariant broken"
 
-expose :: a -> Forest a -> (Path a,Forest a)
-expose x f = let
-    (unzipResults -> (rs,ts),f') = exposeIterate (Just x) ([],f)
-    root = joinAndReversePaths ts -- :: Path a
-    in second (rs ++) $ mergeCorrection root f' 
+exposeIterate :: Ord a => Maybe a -> Forest Path a -> 
+    ([Result a], Forest Path a)
+exposeIterate mf f = second snd $ unfoldrE exposeCore (mf,f)
+
+expose :: Ord a => a -> (Path a -> Path a) -> Forest Path a -> Forest Path a
+expose x c f = let
+    (unzipResults -> (rs,ts),f') = exposeIterate (Just x) f
+    in rs ++ mergeOrInsert (c $ joinAndReversePaths ts) f' 
     
-link :: a -> a -> Forest a -> Forest a
-link x y f = let 
-    (Path Nothing p, f') = expose y f
-    in case takePath LC x f' of
-        (Right _,_) -> Path (Just x) p : f'
-        (Left (Path mf p'),f'') -> Path mf (p <> p') : f''
-
+link :: Ord a => a -> a -> Forest Path a -> Forest Path a
+link x y f = let
+    (unsplitPath -> s, f') = selectPath x f -- future father
+    in mergeOrInsert s $ expose y (set father $ Just x) f' 
+    
+{-
 cut :: a -> Forest a -> Maybe (Forest a)
 cut x f = case takePath RC x f of
         (Left _, _) -> Nothing
